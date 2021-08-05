80dc165866e4360252874144fd7d9ac844e9d4b8
Theodore Frazier <theo.frazier@icloud.com>
2021-08-05 17:06:29 -0400

    hasUserLost function created

--- src/snowman.js
+++ src/snowman.js
@@ -22,9 +22,95 @@ function getRandomWord() {
 
   Once you understand the code below, you may remove the comments if you like.
 */
+
+/* -------------------------------------------------------------------   HELPER FUNCTIONS -------------------------------------------------------------------- */
+
+/*
+  FUNCTION: HAS USER LOST?
+
+    This function will take the take the number the letter the user has guessed and compared it to the maximum numbers of letters allowed.
+    
+    If the maximum number of guesses have been reached, the game will be stopped and the user will be asked if they want to play again.
+
+    If 'n' the game will end. If 'y', the game will restart. 
+
+*/
+
+
+
+    function hasUserLost() {
+
+      if (lettersGuessed === maxLetterGuess) {
+        gameRecord.gamePlay = false;
+        console.log("You've reached the max numbers of guessed allowed. You LOSE!")
+        let playAgain = readline.question("There's always another race! \n Do you want to try again? (Y or N) ").toLowerCase();
+  
+        if (playAgain === "n" || playAgain === "N") {
+          console.log("Okay, thank you for playing! Logging out.......");
+        }
+        else {
+          console.log("Okay great! Rev your engines and lets start again!");
+          gamePlay();
+        }
+      }
+  
+    }
+
+
+
+
+
+
 function run() {
+
+  //Object to store game status 
+  let gameRecord = {
+
+    //is the game active?
+    gameActive: true,
+
+    //MaxLettersToGuess = 10
+    maxLetterGuess = 10,
+
+    //Letters guessed so fall
+    lettersGuessed = 0,
+
+    //Word guessed correctly
+    wordsGuessed: 0,
+
+    //Words guessed incorrectly 
+    wordsNotGuessed: 0,
+
+  }
+
+
+  while (state.gameActive) {
+
+    //Ask the user if they are ready to play
+    let introGame = readline.question("Welcome to the car maker guessing game. In this game you will be guessing the name of varius car makers. \n Are you ready to play?(Y or N) ").toLowerCase();
+
+    //
+    if (introGame === "n" || introGame === "no") {
+      console.log("Okay, please come back when you are ready!")
+      gameRecord.gameActive = false;
+    }
+    else {
+      console.log("Start your engines.......3, 2, 1. GO!!!")
+      //Begin the game play
+      gamePlay();
+    }
+
+  }
+
+
   // This line of code gets a random word. The `word` variable will be a string.
-  const word = getRandomWord();
+  const word = getRandomWord()
+
+  console.log("Random car name is generating.....");
+  console.log("Okay we're ready! Guess a letter, hit return, and see if it appears below. Keep guessing until you get the can see what car is generated!");
+  getRandomWord()
+
+
   /*
     The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
 
@@ -32,9 +118,13 @@ function run() {
 
     After a user hits the 'return' key, the rest of the code will run.
   */
+
   const userInput = readline.question("Guess a letter: ");
   // This line of code will print out whatever is inputted in by the user.
   console.log("THE USER INPUTTED:", userInput);
+
+
+
 }
 
 run();

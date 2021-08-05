58862307ce265700e106eaefbe4dd93d644d1962
JoseBorbon <joserborbon@gmail.com>
2021-08-05 19:00:49 -0400

    added getNonRepeatingLetterJS

---
+++ src/getNonRepeatingLetter.js
@@ -0,0 +1,17 @@
+const getValidLetterGuess = require("./getValidLetterGuess.js");
+
+//This function returns a letter that wasn't guessed already
+function getNonRepeatingLetter(lettersGuessed) {
+  //while letterGuess from user input is included in lettersGuessedArray
+  let userInput = getValidLetterGuess();
+  while (lettersGuessed.includes(userInput)) {
+    //if the guess is included in letter guesses have the user type in another guess
+    if (lettersGuessed.includes(userInput)) {
+      console.log("\nYou already guessed this letter: ", userInput + "\n" + "These are all your guesses: ", lettersGuessed.join(" ") + "\nTry Again!\n");
+      userInput = getValidLetterGuess();
+    }
+  }
+  return userInput;
+}
+
+module.exports = getNonRepeatingLetter;

102132de1052606a0d65874c555eaf326e023a31
DylanLovell <arcanyne7@gmail.com>
2021-08-05 16:06:29 -0400

    imported list of valid inputs, and psuedocode

--- src/snowman.js
+++ src/snowman.js
@@ -14,7 +14,14 @@ function getRandomWord() {
   const index = Math.floor(Math.random() * dictionary.length);
   return dictionary[index];
 }
+//inporting valid inputs
+const validInput = require("./validinput");
 
+// New helper function
+function validInputCheck(letter){
+  //Check if letter is valid and return it
+  return validInput.includes(letter.toLowerCase())
+};
 /*
   This function will run your game. Everything you want to happen in your game should happen inside of here.
 
@@ -23,18 +30,29 @@ function getRandomWord() {
   Once you understand the code below, you may remove the comments if you like.
 */
 function run() {
-  // This line of code gets a random word. The `word` variable will be a string.
-  const word = getRandomWord();
-  /*
-    The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
-
-    The text that will show up to the user will be "Guess a letter: ". Whatever value is entered will be assigned to the variable `userInput`.
-
-    After a user hits the 'return' key, the rest of the code will run.
-  */
+  //Declare variable for our random word
+  const secretWord = getRandomWord();
+  //Declare variable for our incorrectGuess, set to length of secretWord
+  //Declare variable for alreadyGuessed, set to empty array
+  //Declare variable for displayedWord, set to empty array
+  //Use a loop to fill our displayedWord with "_"s, make it as long as our secretWord
+  
+  //Create loop for our game
+  //Print our game
+  //Ask for an input
   const userInput = readline.question("Guess a letter: ");
-  // This line of code will print out whatever is inputted in by the user.
-  console.log("THE USER INPUTTED:", userInput);
+  //check if input isnt valid with helper function
+    //if it isn't, ask user to input valid input.
+  //check if input was already guess
+    //if it was, tell the user, and don't penalize them
+  //loop through our secretWord
+    //in loop, check if input = currentLetter
+      //if it is, change the value of displayedWord[i], to currentLetter
+  //after loop, check if displayWord includes input
+    //if it does, incorrect Guess -= 0;
+    //if it isn't, incorrectGuess -= 1;
+  //Add input to letters guessed
+  
 }
 
 run();

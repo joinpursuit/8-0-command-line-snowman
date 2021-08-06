ca856826a07aa1e2e994ac9da447095e6bdb1b44
JoseBorbon <joserborbon@gmail.com>
2021-08-06 12:52:53 -0400

    updated getWinOrLoss to remove logSnowManFunc

--- src/getWinOrLoss.js
+++ src/getWinOrLoss.js
@@ -1,19 +1,19 @@
 /*
+  `readline-sync` is a library that allows you to access user input from the command line. The library is assigned to the variable `readline`. It is used in the `run()` function below.
+*/
+const readline = require("readline-sync");
+/*
   The `getNonRepeatingLetter` variable is a function that when invoked will return a letter that has not been repeated
 */
 const getNonRepeatingLetter = require("./getNonRepeatingLetter.js");
-/*
-  The `logSnowMan` variable is a function that when invoked will log a snowman to the console
-*/
-const logSnowMan = require("./logSnowMan.js");
 
 function getWinOrLoss(word) {
   //declare a variable called currentWordState and assign it an array filled with underscores depending on length of the word
   const currentWordState = new Array(word.length).fill("_"); //[_,_,_,_,_]
   const lettersGuessed = []; // put inside game state
   let remainingGuesses = 7; // put inside game state
-  logSnowMan();
-  console.log("☃️  Welcome To Snowman! ☃️\n\nRemaining Guesses: ", remainingGuesses + "\nWord: ", currentWordState.join(" ") + "\n");
+
+  console.log("Remaining Guesses: ", remainingGuesses + "\nWord: ", currentWordState.join(" ") + "\n");
   //while we still have remaining guesses
   while (remainingGuesses > 0) {
     if (word === currentWordState.join("")) return true;
@@ -31,10 +31,12 @@ function getWinOrLoss(word) {
           //assign currentWordState at index to userInput
           currentWordState[index] = userInput;
           console.log("\nThat's correct!");
+          // readline.question("Hit Enter key to continue.", { hideEchoBack: true, mask: "" });
         }
       });
     } else {
-      console.log("\n That's incorrect, The word " + userInput + "is not in the word.");
+      console.log("\nThat's incorrect, The letter" + " '" + userInput + "' " + "is not in the word.");
+      readline.question("Hit Enter key to continue.", { hideEchoBack: true, mask: "" });
       remainingGuesses--;
     }
     //post iteration log to the console Remaining Guesses, LettersGuessed comma space seperated, and currentWordState using join method space seperated
--- src/snowman.js
+++ src/snowman.js
@@ -6,13 +6,19 @@ const getRandomWord = require("./getRandomWord.js");
   The getWinOrLoss variable is a function that will return whether or not the user won in SnowMan
 */
 const getWinOrLoss = require("./getWinOrLoss.js");
+/*
+  The displayMainMenu variable is a function that will display a menu of options to the user before choosing to play the game or exiting out
+*/
+const displayMainMenu = require("./displayMainMenu.js");
 
 //used IIFE to run snowMan on the fly
 (function runSnowman() {
   // This line of code gets a random word. The `word` variable will be a string.
   const word = getRandomWord();
-  // Declare a variable called userIsWinner and assign it the evaluated result of invoking gameState
+
+  displayMainMenu();
   const userIsWinner = getWinOrLoss(word);
+  // Declare a variable called userIsWinner and assign it the evaluated result of invoking gameState
   if (userIsWinner) {
     console.log("\n🏆 You Won! 🏆\nThe word was: " + word + "!\n🌟 You're a star! 🌟");
   } else {

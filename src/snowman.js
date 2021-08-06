3391d8d757f267e2da0515c6df776bb8bfe09804
DylanLovell <arcanyne7@gmail.com>
2021-08-06 16:31:21 -0400

    added username and updated format

--- src/snowman.js
+++ src/snowman.js
@@ -30,6 +30,8 @@ function validInputCheck(letter){
   Once you understand the code below, you may remove the comments if you like.
 */
 function run() {
+  //Ask for their name
+  const userName = readline.question("Enter your username!") || "User";
   //Declare variable for our secretword
   const secretWord = getRandomWord();
   //Declare variable for our incorrectGuess, set to length of secretWord
@@ -43,24 +45,25 @@ function run() {
     displayedWord.push("_");
   }
   //Declare variable for message
-  let message = "";
+  let message = `\n\nHello ${userName}! Enjoy your game!`;
+
   //Create loop for our game
   while (incorrectGuess !== 0 && displayedWord.includes("_")){
     //Print our game
-    console.log(`Remaining Incorrect Guesses: ${incorrectGuess}\nLetters Guessed: ${alreadyGuessed.join(", ") || "None"}\nWord: ${displayedWord.join(" ")}${message}`);
+    console.log(`\n-------------------------------------\nRemaining Incorrect Guesses: ${incorrectGuess}\nLetters Guessed: ${alreadyGuessed.join(", ") || "None"}\nWord: ${displayedWord.join(" ")}${message}`);
     //Ask for an input
     const userInput = readline.question("Guess a letter: ");
     //check if input isnt valid with helper function
     if (!validInputCheck(userInput)){
       //if it isn't, ask user to input valid input
-      message = `\n\nInvalid input of ${userInput}. Please enter a single letter.\n-------------------------------------\n`
+      message = `\n\n${userName}, you submitted an invalid input of ${userInput}. Please enter a single letter.\n-------------------------------------\n`
       //send them back to the guess phase
       continue;
     }
     //check if input was already guess
     if (alreadyGuessed.includes(userInput)){
       //if it was, tell the user, and don't penalize them
-      message = `\n\nInput of ${userInput} has already been guessed.\n-------------------------------------\n`;
+      message = `\n\n${userName}, your input of ${userInput} has already been guessed.\n-------------------------------------\n`;
       continue;
     }
     //loop through our secretWord
@@ -77,11 +80,11 @@ function run() {
     //after loop, check if displayWord includes input
     if (displayedWord.includes(userInput)){
       //if it does, change message
-      message = `\n\nYou guessed correctly!\n-------------------------------------\n`;
+      message = `\n\nWell done ${userName}! You guessed correctly!\n-------------------------------------\n`;
     } else {
       //if it isn't, incorrectGuess -= 1;
       incorrectGuess -= 1;
-      message = `\n\nYou guessed wrong :c\n-------------------------------------\n`
+      message = `\n\nSorry ${userName}, you guessed wrong :c\n-------------------------------------\n`
     }
      //Add input to letters guessed
      alreadyGuessed.push(userInput);
@@ -91,10 +94,10 @@ function run() {
   //check if they won
   if (!displayedWord.includes("_")){
     //if they did, give winning message
-    console.log(`\n\nWINNER\n-------------------------------------\n`);
+    console.log(`\nGOOD JOB ${userName}!!! YOU'RE A WINNER!!\nYour word was: ${secretWord}\n\n-------------------------------------\n`);
   } else {
     //if they didn't, give losing message
-    console.log(`\n\nLOSER! the word was ${secretWord}\n-------------------------------------\n`);
+    console.log(`\nSorry ${userName}, better luck next time! Your word was: ${secretWord}\n\n-------------------------------------\n`);
   }
 }
 

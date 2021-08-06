5fc2017155fbc03a00ede0b2c1493b193114ce0d
Cristobal Arias <cristobal.arias.13@gmail.com>
2021-08-06 11:10:06 -0400

    Completely functional game, needs some refinement

--- src/snowman.js
+++ src/snowman.js
@@ -25,6 +25,16 @@ function getRandomWord() {
 function run() {
   // This line of code gets a random word. The `word` variable will be a string.
   const word = getRandomWord();
+
+  //declare empty string variable called "dashes"
+  let dashes = ''
+
+  //iterate through "word" string, each element is called "letter"
+  for(const letter of word){
+    //add an underscore to "dashes" (sort of a misnomer, but it stuck) 
+    dashes += '_'
+  }
+
   /*
     The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
 
@@ -32,9 +42,64 @@ function run() {
 
     After a user hits the 'return' key, the rest of the code will run.
   */
-  const userInput = readline.question("Guess a letter: ");
-  // This line of code will print out whatever is inputted in by the user.
-  console.log("THE USER INPUTTED:", userInput);
+
+    //Declare an empty array called "guessesArray" 
+  let guessesArray = []
+
+    //Declare a variable called "tries", set it equal to 7, so that we can decrement down for the number of tries left.
+  let tries = 7
+
+  //keep looping if the user has tries left, or if they haven't successfully guessed the full word yet.
+
+  while(tries > 0 && dashes !== word){
+    console.clear()
+    console.log(word)
+    console.log(dashes)
+    //print whatever the user inputted
+    console.log("YOU GUESSED: ", guessesArray.join(', '))
+    console.log(`You have ${tries} guesses remaining`)
+
+    const userInput = readline.question('Guess a letter: ')
+
+    const userInput2 = userInput.toLowerCase()
+
+    if(userInput2.match(/[a-z]/g) !== null && userInput2.length === 1){
+      if(word.includes(userInput2)){
+        let arrDashes = dashes.split('')
+        for(const i in word){
+          if(userInput2 === word[i]){
+            arrDashes.splice(i, 1, word[i])
+          }
+        }
+        dashes = arrDashes.join('')
+      }else if(!word.includes(userInput2) && !guessesArray.includes(userInput2)){
+        tries--
+      }
+
+      //compare "userInput2" to "guessesArray" using .includes() method.
+      if(guessesArray.includes(userInput2)){
+        console.log(`You've guessed ${userInput2} already, try again.`)
+      }else{
+        //if it evaluates to false, push "userInput2" into "guessesArray"
+        guessesArray.push(userInput2)
+      }
+    }else{
+      console.log(`"${userInput2}" is not a valid input`)
+      readline.question('Press enter to continue...')
+    }
+  }
+
+  console.clear()
+
+  console.log(`The word was ${word}`)
+
+  if(dashes === word){
+    console.log('Congratulations! You won!')
+  }else{
+    console.log(`You/'ve run out of tries, try again.`)
+  }
+
+  
 }
 
 run();

ec0cbfff6370c77b97ce825ca3e67dd338e4b1c6
rae charles <r_charles87@yahoo.com>
2021-08-06 07:55:04 -0400

    games plays but still making changes

--- src/snowman.js
+++ src/snowman.js
@@ -2,19 +2,11 @@
   `readline-sync` is a library that allows you to access user input from the command line. The library is assigned to the variable `readline`. It is used in the `run()` function below.
 */
 const readline = require("readline-sync");
-/*
-  The `dictionary` variable will have an array of words that can be used for your game. It is used in the `getRandomWord()` function.
-*/
 const dictionary = require("./dictionary");
-
-/*
-  This function returns a random word from the list in `src/dictionary.js`. You do not need to update or edit this function. Instead, you only need to call it from the `run()` function.
-*/
-function getRandomWord() {
+function getRandomWord(dictionary) {
   const index = Math.floor(Math.random() * dictionary.length);
   return dictionary[index];
 }
-
 /*
   This function will run your game. Everything you want to happen in your game should happen inside of here.
 
@@ -23,18 +15,74 @@ function getRandomWord() {
   Once you understand the code below, you may remove the comments if you like.
 */
 function run() {
-  // This line of code gets a random word. The `word` variable will be a string.
-  const word = getRandomWord();
-  /*
-    The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
+  const word = getRandomWord(dictionary);
+  let answerArr = [];
+  for (let i = 0; i < word.length; i++){
+    answerArr[i] = "_";
+  };
 
-    The text that will show up to the user will be "Guess a letter: ". Whatever value is entered will be assigned to the variable `userInput`.
-
-    After a user hits the 'return' key, the rest of the code will run.
-  */
-  const userInput = readline.question("Guess a letter: ");
-  // This line of code will print out whatever is inputted in by the user.
-  console.log("THE USER INPUTTED:", userInput);
+  let guessArr = [];
+  let lives = word.length
+  lives = lives + 2;
+  let remainingLetters = word.length;
+  
+  while(remainingLetters > 0){
+    console.log(answerArr.join(" "))
+    if (lives == 1){
+      console.log("you have 1 life left")
+    }
+    else if(lives > 1){
+      console.log("You have" + lives + "lives left." + "\n")
+    }
+    const userInput = readline.question("Guess a letter: "); 
+    if (guessArr.includes(userInput)){
+      console.log("\n" + "Already chosen.")
+    }
+    console.log("THE USER INPUTTED:", userInput);
+    guessArr.push(userInput)
+    if(userInput === null){
+      break;
+    }
+    else if(userInput.length !== 1){
+      console.log("Please enter a single letter.")
+    }
+    else if (!word.includes(userInput)){
+      lives--
+      console.log("Another life bites the dust.")
+    }
+    else if (lives == 0){
+      console.log("you lost bro.")
+    }
+    else {
+      for (let j = 0; j < word.length; j++){
+        if (word[j] === userInput){
+          answerArr[j] = userInput
+          remainingLetters--
+        }
+      }
+    }
+    if (lives == 0){
+      console.log( "\n" +"You lost bro.")
+      break;
+    }
+  }
+  if (answerArr == guessArr){
+  console.log(answerArr.join(" "))
+  console.log("Look at you knowing how to spell the answer was" + " " + word + "!")
+  }
+  else{
+  console.log(answerArr.join(" "))
+  console.log("Damn I thought you could spell the word was" + " " + word + "!")
+  }
 }
 
 run();
+
+/*
+The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
+
+The text that will show up to the user will be "Guess a letter: ". Whatever value is entered will be assigned to the variable `userInput`.
+
+After a user hits the 'return' key, the rest of the code will run.
+*/
+// This line of code will print out whatever is inputted in by the user.

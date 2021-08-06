573753343f1d7389beb45ee30bb1c01cd74377a5
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-08-06 19:24:17 -0400

    first functioning game

--- src/snowman.js
+++ src/snowman.js
@@ -14,17 +14,140 @@ function getRandomWord() {
   const index = Math.floor(Math.random() * dictionary.length);
   return dictionary[index];
 }
+let word = getRandomWord();
 
-/*
-  This function will run your game. Everything you want to happen in your game should happen inside of here.
-
-  You should still define other, smaller functions outside of the `run()` function that have a single specific purpose, such as getting user input or checking if a guess is correct. You can then call these helper functions from inside the `run()` function.
-
-  Once you understand the code below, you may remove the comments if you like.
-*/
 function run() {
-  // This line of code gets a random word. The `word` variable will be a string.
-  const word = getRandomWord();
+let snowman = {
+        arrayChosenWord: [],
+        letter: '',
+        hiddenWord: '',
+        arrayHiddenWord: [],
+        guessedWords: '',
+        arrayGuessedWords: [],
+        guessLeft: 8,
+        solvedWord: '',
+        arrayCurrentSolvedWord: []
+      }
+      //to start the game
+  let choice = readline.keyInYN('👺 Hi! Would you like to play a game? 👺')
+    if(choice) {
+      console.log("\nWelcome! Let's start!")
+      //function here to show hiddenWord to user
+      makeTheBlanks()
+      //function here for user to play the game
+      gamePlay()
+      } else {
+        console.log('Oh well! I tried')
+          quitTest()
+      }
+//quit the test after the user is done
+function quitTest() {
+        console.log("Goodbye!")
+        //stop program from running
+        process.exit()
+      }
+//replace the hidden word with letters if guessed correctly
+function gamePlay() {
+  //make the function run until no guesses left
+    while (snowman.guessLeft !== 0) {
+  getValidLetterGuess()
+    snowman.arrayChosenWord = word.split(''); 
+    snowman.arrayHiddenWord = snowman.hiddenWord.split('');
+    //add letter to hidden word if correct letter is given
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === snowman.letter) {
+          snowman.arrayHiddenWord[i * 2] = snowman.arrayChosenWord[i];
+          snowman.arrayCurrentSolvedWord[i] = snowman.arrayChosenWord[i];
+            }
+          }
+          //negate the subtraction from receiving a guess by adding a point to guessLeft if a correct guess is given
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === snowman.letter) {
+          snowman.guessLeft = snowman.guessLeft + 1
+            break;
+          }
+        }
+          //subtract from guessLeft when wrong guess is given
+          snowman.guessLeft = snowman.guessLeft - 1;
+          word = snowman.arrayChosenWord.join(''); 
+          snowman.hiddenWord = snowman.arrayHiddenWord.join('');
+          snowman.solvedWord = snowman.arrayCurrentSolvedWord.join('');
+      //winning message when word is guessed
+      if (snowman.solvedWord === word) {
+          console.log("Congratulations! 🎉 🎊 🥳")
+          //print chosenWord for user to see
+          console.log('The word was ' + '"' + word + '"')
+          //function here to show ending message and stop if user wins
+          quitTest() 
+        }
+          //print hidden word for user
+          console.log('WORD: '+ snowman.hiddenWord)
+          //function here to show user the letters that are already guessed
+          showGuessedWord()
+        }
+        //function here for user to know how many guesses left
+        guessLeftCounter()
+      }
+//show the letters the user already guessed
+function showGuessedWord() {
+    if (snowman.arrayGuessedWords.length === 0) {
+      snowman.arrayGuessedWords.push(snowman.letter)
+    } else {
+      snowman.arrayGuessedWords.push((', ' + snowman.letter))
+    }
+    snowman.guessedWords = snowman.arrayGuessedWords.join('')
+      //prints guessed letters for user to see 
+      console.log('\nGuessed Letters: ' + snowman.guessedWords)
+    }
+//count how many guesses the user has left until loses
+function guessLeftCounter() {
+  console.log("\nYou have " + snowman.guessLeft + " guess(es) remaining")
+    // print losing message if user has no more guesses left
+    if (snowman.guessLeft === 0) {
+      console.log('\nGAME OVER, 😔 better luck next time!')
+      //prints chosenWord for the losing user
+      console.log('The word was ' + '"' + word + '"')
+        //function here to show ending message and stop if user loses
+        quitTest()
+      }
+    }
+//turns the dictionary word into a hidden word
+function makeTheBlanks() {
+  //assign value for getRandomWord() to chosenWord
+  
+  // snowman.chosenWord = word
+  snowman.hiddenWord = ''
+    // iterate through chosen word to determine the hiddenWord
+    for (let i = 0; i < word.length; i++) {
+      if (i === 0) {
+        snowman.hiddenWord += '_'
+      } else {
+     snowman.hiddenWord += ' _'
+    }
+  }
+  //prints the hiddenWord and the chosenWord(hint)
+  console.log('WORD: ' + snowman.hiddenWord + ` (${word})`)
+} 
+//checks for a valid guess
+function getValidLetterGuess() {
+  //function here for user to know how many guesses left
+  guessLeftCounter()
+//same inputted letters and numbers are invalid 
+function guessIsValid(letterGiven) {
+  return (letterGiven.length === 1) && (letterGiven.toUpperCase() !== letterGiven.toLowerCase()) && (!(snowman.guessedWords.includes(letterGiven)))
+}
+  snowman.letter = ""
+    while (!snowman.letter) {
+  const userInput = readline.question("\nGuess a letter: ");
+      if (guessIsValid(userInput)) {
+        snowman.letter = userInput
+      } else {
+        console.log("Please enter a valid letter")
+    }
+  }
+}
+    
+
   /*
     The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
 
@@ -33,8 +156,10 @@ function run() {
     After a user hits the 'return' key, the rest of the code will run.
   */
   const userInput = readline.question("Guess a letter: ");
-  // This line of code will print out whatever is inputted in by the user.
+  
+  // This line of code will print out whatever is inputted in by the user:
   console.log("THE USER INPUTTED:", userInput);
+  
 }
 
-run();
+  run();

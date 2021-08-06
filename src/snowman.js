60dd6f91988577ca6049ebe91512bd6b90f45f66
salrey <tekdehn@gmail.com>
2021-08-05 21:52:48 -0400

    Added helper functions for errors, letters guessed, hidden word and remaining guesses

--- src/snowman.js
+++ src/snowman.js
@@ -2,39 +2,118 @@
   `readline-sync` is a library that allows you to access user input from the command line. The library is assigned to the variable `readline`. It is used in the `run()` function below.
 */
 const readline = require("readline-sync");
-/*
-  The `dictionary` variable will have an array of words that can be used for your game. It is used in the `getRandomWord()` function.
-*/
+
 const dictionary = require("./dictionary");
 
-/*
-  This function returns a random word from the list in `src/dictionary.js`. You do not need to update or edit this function. Instead, you only need to call it from the `run()` function.
-*/
 function getRandomWord() {
   const index = Math.floor(Math.random() * dictionary.length);
   return dictionary[index];
 }
 
-/*
-  This function will run your game. Everything you want to happen in your game should happen inside of here.
 
-  You should still define other, smaller functions outside of the `run()` function that have a single specific purpose, such as getting user input or checking if a guess is correct. You can then call these helper functions from inside the `run()` function.
+//----------- GAME STATE ----------------
 
-  Once you understand the code below, you may remove the comments if you like.
-*/
+//HELPER FUNCTIONS 
+
+//Set up guard clauses 
+function checkInput(input) {
+  if ((input >= 'a' && input <= 'z') && input.length - 1 === 0 || (input >= 'A' && input <= 'Z') && input.length - 1 === 0) {
+    if (checkArray.includes(input)) {
+      console.log("\n Error! You already guessed that. Input a different letter") 
+      return true
+    }
+  } else {
+    console.log("\n Error! Input a letter") 
+    return true
+  }
+  checkArray.push(input)
+} 
+
+//List of letters guessed
+function lettersList(input) {
+  if ((input >= 'a' && input <= 'z') && input.length - 1 === 0 || (input >= 'A' && input <= 'Z') && input.length - 1 === 0) {
+    letterArray.push(input.toUpperCase()) 
+  } 
+  return letterArray.join(", ")
+} 
+
+// The Word hidden in underscore
+function hiddenLetters(word, input) {
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i]
+    if (input === letter) {
+      hiddenArray[i] = letter
+    } else if (!hiddenArray[i]) {
+      hiddenArray[i] = "_"
+    }
+    hiddenWord = hiddenArray.join(" ") 
+  }
+  return hiddenWord
+}
+
+// Remaining Incorrect Guesses
+function guessesLeft (guesses, word, input) {
+  if (!word.includes(input)) {
+    guesses--
+  } else {
+    guesses
+  }
+  return guesses
+}
+
+// Set up global variables to access throughout
+const word = getRandomWord();
+let guesses = 7  
+let guessCount = 0
+let checkArray = []
+let letterArray = []
+let hiddenArray = []
+let hiddenWord = ""
+
 function run() {
-  // This line of code gets a random word. The `word` variable will be a string.
-  const word = getRandomWord();
-  /*
-    The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
+  //Check the word to test
+  //console.log(word)
+  
+  console.log("----------The Snowman Game----------\n\n Remaining Incorrect Guesses: "  + guesses + "\n Word: " + hiddenLetters(word) + "\n\n") 
+  
+  //While loop until word is complete or there are no more guesses 
+  while (word !== hiddenWord && guesses > 0) {
+    let userInput = readline.question(" Guess a letter: ");
+    
+    //Check for Errors
+    while (checkInput(userInput)) {
+      userInput = readline.question(" Guess a letter: ")
+    }
+    
+    //Count number of guesses taken
+    if (guessesLeft(guesses, word, userInput) !== guesses) {
+      guessCount += 1 
+    }
 
-    The text that will show up to the user will be "Guess a letter: ". Whatever value is entered will be assigned to the variable `userInput`.
+    //Update remaining incorrect guesses
+    guesses = guessesLeft(guesses, word, userInput)
+    
+    //Update letters guessed
+    lettersGuessed = lettersList(userInput)
 
-    After a user hits the 'return' key, the rest of the code will run.
-  */
-  const userInput = readline.question("Guess a letter: ");
-  // This line of code will print out whatever is inputted in by the user.
-  console.log("THE USER INPUTTED:", userInput);
+    //Update hidden word
+    hiddenWord = hiddenLetters(word, userInput)
+
+    console.log("-----------------------------------")
+    console.log("\nRemaining Incorrect Guesses: " + guesses + "\nLetters Guessed: " + lettersGuessed + "\n Word: " + hiddenWord + "\n\n")
+    
+    //Set up to compare hidden word with dictionary word
+    hiddenArray = hiddenWord.split(" ")
+    hiddenWord = hiddenArray.join("")
+  }
+  
+  //Check if you won or lost
+  if (word === hiddenWord) {
+    console.log("You win! You took " + guessCount + " guesses\n")
+  } else {
+    console.log("You lost! The correct word is " + word + "\n")
+  }
+
 }
 
 run();

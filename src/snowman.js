8a158cbb6a2bc770143d5bda30102845f5098e50
Fatema Kabir <fatema.kabir@pursuit.org>
2021-08-09 13:28:08 -0400

    congratulate the player on the win

--- src/snowman.js
+++ src/snowman.js
@@ -22,9 +22,68 @@ function getRandomWord() {
 
   Once you understand the code below, you may remove the comments if you like.
 */
+//follow
+//012345
+//_ _ _ _ _ _
+//0123456789t
+//0 1 2 3 4 5
+
+
 function run() {
   // This line of code gets a random word. The `word` variable will be a string.
-  const word = getRandomWord();
+  const word = "good"
+  //getRandomWord();
+  let winMessage = "Yay!"
+  let incorrectGuesses = word.length + 1
+  let lettersGuessed = ""
+  let theBlanks = ""
+  for (let i = 0; i < word.length; i++){
+    theBlanks += "_ "
+  }
+
+  console.log("Remaining Incorrect Guesses: " + incorrectGuesses)
+  console.log("Letters Guessed: " + lettersGuessed)
+  console.log("Word: " + theBlanks)
+//game ends when someone wins
+//once they guess the correct word = win
+// _ _ _ _
+// g o o d
+//if there are no blanks remaining the game ends
+//if (theBlanks.includes("_") )
+
+//neverending loop
+  for (let i = 0; i < word.length; i++){
+//ask for userinput
+    let userInput = readline.question("Pick a letter: ")
+  //the loop that matches the guesses   
+  for (let i = 0; i < word.length; i++){
+    if(userInput === word[i]){
+      theBlanks = theBlanks.split("")
+      theBlanks.splice(i*2, 1, userInput)
+      theBlanks = theBlanks.join('')
+    }
+  } 
+  console.log("Remaining Incorrect Guesses: " + incorrectGuesses)
+  console.log("Letters Guessed: " + lettersGuessed)
+  console.log("Word: " + theBlanks)
+  
+    console.log("Letter guessed:", userInput);
+      if(!theBlanks.includes("_")) {
+        
+       return console.log (winMessage);
+  
+//decrement remainingg incorrect guesses each time user enters a wrong guess
+
+     
+      } else {
+      i--
+    }
+  }
+
+
+  
+
+
   /*
     The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
 
@@ -32,9 +91,23 @@ function run() {
 
     After a user hits the 'return' key, the rest of the code will run.
   */
-  const userInput = readline.question("Guess a letter: ");
-  // This line of code will print out whatever is inputted in by the user.
-  console.log("THE USER INPUTTED:", userInput);
-}
 
-run();
+
+  // console.log("Remaining Incorrect Guesses: " + incorrectGuesses)
+  // console.log("Letters Guessed: " + lettersGuessed)
+  // console.log("Word: " + theBlanks)
+  // // console.log (typeof theBlanks)
+
+
+
+
+  // // This line of code will print out whatever is inputted in by the user.
+  // console.log("Letter guessed:", userInput);
+  
+}
+ 
+ run();
+  
+
+
+//how do I get the code to keep running

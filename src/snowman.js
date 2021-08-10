aed070a6bbc679e5442b0be3bb04ff9c24130ed6
Jalamang Janka <jalamangjankagmail.com>
2021-08-09 22:13:54 -0400

    Thought Processs: Guide to  how the game is played

--- src/snowman.js
+++ src/snowman.js
@@ -1,3 +1,40 @@
+/** 
+||The Snowman Game
+
+||Global Variables
+
+let guessedWord {[]} // stores the guess letters forming the secret word
+let guessedLetter {string} //letter guessed to form the word 
+let WordLength {number}. // length of the word to be guessed
+let alreadyGuessedLetters {[]} //stores letters already guessed
+
+||The Game flow process
+
+=>  Randomly gets a word to be guessed.
+1)  Declare an empty array called guessedWord = [].
+2)  Do a for loop on the random word called word in order to populate the empty array, 
+    guessedWord {[]}, with '_' and having the same array size as guessedWordn.
+3)  Declare a varaible called WordLength to keep track of the game progress.
+    =>  Game starts:
+    =>  while the given WordLength greater than zero.
+        
+        a)  Iterate the the given word and compare each letter with the user's guessedLetter
+        b)  if they are the same and the same letter not found in alreadyGuessedLetters array,
+            i) Assign the guessedLetter to the corresponding index in the guessedWord array
+            ii) push a guessedLetter copy in alreadyGuessedLetters array.
+            iii) if the same letter is found in alreadyGuessedLetters array, display an error msg
+            iv) if guessedWord is the same as the randomly given word, the user is declared the winner
+        c) Otherwise if guessedLetter is incorrect, decrement the WordLength by 1 
+        b)  keep asking the user to guess a letter in the word until there is no chance to guess.
+
+=> Edge Cases (invalid input decrease the chance by 1 after each play)
+if user's input, guessedLetter is equal to ''/null/undefined, display message
+if user's input, guessedLetter.length is not equal to 1, or a number, ask the user to type a single 
+letter and that a number or symbol is not allowed
+
+*/
+
+
 /*
   `readline-sync` is a library that allows you to access user input from the command line. The library is assigned to the variable `readline`. It is used in the `run()` function below.
 */
@@ -15,11 +52,14 @@ function getRandomWord() {
   return dictionary[index];
 }
 
-/*
-  This function will run your game. Everything you want to happen in your game should happen inside of here.
-  You should still define other, smaller functions outside of the `run()` function that have a single specific purpose, such as getting user input or checking if a guess is correct. You can then call these helper functions from inside the `run()` function.
-  Once you understand the code below, you may remove the comments if you like.
-*/
+
+
+
+
+
+
+
+
 function run() {
   // This line of code gets a random word. The `word` variable will be a string.
   const word = getRandomWord();

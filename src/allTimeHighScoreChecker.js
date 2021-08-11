36c564fabbeb80f4e7cc0e3eb83c251e0c995ff8
JoseBorbon <joserborbon@gmail.com>
2021-08-10 23:04:21 -0400

    removed allTimeHighScoreCheckerJS

--- src/allTimeHighScoreChecker.js
+++
@@ -1,7 +0,0 @@
-function allTimeHighScoreChecker(name, currentSessionScore) {
-  const fileSync = require("fs");
-  //declare a constant variable called scores to get highScores Obj
-  const scores = JSON.parse(fileSync.readFileSync("./src/highScores.json"));
-}
-
-module.exports = allTimeHighScoreChecker;

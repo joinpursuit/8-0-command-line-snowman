844a1633fbce4e878db2df23fd92201e7ca19d95
Nima sherpa <Toplouie78@gmail.com>
2021-08-11 10:07:56 -0400

    Snow man game

--- src/snowman.js
+++ src/snowman.js
@@ -9,88 +9,76 @@ const dictionary = require("./dictionary");
 /*
   This function returns a random allV.randWord from the list in `src/dictionary.js`. You do not need to update or edit this function. Instead, you only need to call it from the `run()` function.
 */
+
+// This will give me a random word from a dictionary .
 function getRandomWord() {
   const index = Math.floor(Math.random() * dictionary.length);
   return dictionary[index];
 }
 
+// This helper function check if the word inputted is valid and change it accordingly.
 function replaceSpaceToWord (computerWord,underStr,letter){
   if(computerWord.includes(letter)){
-    underStr = underStr.split("");
-    for (let i = 0; i < computerWord.length; i ++){
-      if(computerWord[i] === letter) {
-        underStr.splice (i , 1 ,letter);
+    underStr = underStr.split(""); // if the word includes letter we split.
+    for (let i = 0; i < computerWord.length; i ++){ // then we loop the word.
+      if(computerWord[i] === letter) { // if it is === to letter.
+        underStr.splice (i , 1 ,letter);// we remove the space and add the letter.
       }
     }
-    return underStr.join("");
+    return underStr.join(""); // then we join the it back together .
   }
   else {
-    return false ;
+    return false ;  // if it doesn't include return nothing. 
   }
 }
 
 function checkWinner(str1,str2) {
   return str1 === str2 ;
 }
-// this helper function is checking if the input is valid 
+
+// this helper function is checking if the input is valid.
 function userInputCheck (userInput){
-  return typeof userInput === "string" && userInput.length === 1
-   && (userInput >= "a" && userInput <= "z");
+  return typeof userInput === "string" && userInput.length === 1 // checks if it is a string and the length is = 1  .
+   && (userInput >= "a" && userInput <= "z"); // the alphabet is = or greater than a and lesser and = to Z
 }
-/*
-  This function will run your game. Everything you want to happen in your game should happen inside of here.
-
-  You should still define other, smaller functions outside of the `run()` function that have a single specific purpose, such as getting user input or checking if a guess is correct. You can then call these helper functions from inside the `run()` function.
-
-  Once you understand the code below, you may remove the comments if you like.
-*/
 
 function run() {
-
   // All The variable we need !!
 
   let allV = {
-     //variable boolean to start the game 
-    startGame : true ,
-    //number of allowed tries 
-    numTries : 7,
-    // keep track of the users
-    inputStr : "",
-    // This line of code gets a random allV.randWord. The `Word` variable will be a string.
-    randWord : getRandomWord(),
+    startGame : true , //variable boolean to start the game 
+    numTries : 7,     //number of allowed tries 
+    inputStr : "",   // keep track of the users
+    randWord : getRandomWord(),  // This line of code gets a random allV.randWord. The `Word` variable will be a string.
+    
   }
-  // //gets correct number of spaces
-  let spaceRepeat = "_".repeat(allV.randWord.length);
+  let spaceRepeat = "_".repeat(allV.randWord.length); //gets correct number of spaces the word has .
+  
 
-  console.log("                     Snow-Man  ")
+  console.log("                     Snow-Man  ") // The game Header .
+  console.log(allV.randWord); // it shows us the word // just for practice.
+  console.log(`     Word : ${spaceRepeat} ` + "\n" );
 
-  // console.log(allV.randWord);
+while(allV.startGame){ 
+   // console logs the game instruction. 
 
-  /*
-    The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
-
-    The text that will show up to the user will be "Guess a letter: ". Whatever value is entered will be assigned to the variable `userInput`.
-
-    After a user hits the 'return' key, the rest of the code will run.
-  */
-
-console.log(`     ${spaceRepeat} ` + "\n" );
-
-while(allV.startGame){
-   //show the player how many underscore.
    console.log("Guess The Letter in Space To Win !!! : ",  [allV.inputStr] + "\n" );
-   console.log(`you have ${allV.numTries} lives -_-  \n `);
+   console.log("* * * * * * * * * * * * * * * * * * * * * *  ");
+   console.log(`You Have ${allV.numTries} Lives Remaining  -_-  \n `); // shows us the remaining lives. 
+   console.log("* * * * * * * * * * * * * * * * * * * * * * ");
+
    //read the user input
-   let userInput = readline.question("You guessed : ")
+   let userInput = readline.question("You guessed --> ")
    // add input to allV.inputStr
    allV.inputStr += ", " + userInput;
    // check if the input is valid 
    if(userInputCheck(userInput)){
      // checking if we have a string or a boolean 
-     let checkIfBoolean = replaceSpaceToWord(allV.randWord, spaceRepeat, userInput )
+     let checkIfBoolean = replaceSpaceToWord(allV.randWord, spaceRepeat, userInput ) // this will see if the user inputted a correct word.
      // check if false , decrement the number of tries
      if(!checkIfBoolean){
        allV.numTries--;
+      console.log(spaceRepeat);
      }
      // change the value of the under string 
     else { 
@@ -98,20 +86,19 @@ while(allV.startGame){
       console.log(spaceRepeat);
      }
     }else {
-    console.log( "\n" + " WRONG!! please answer a valid letter " + "\n" )
+    console.log( "\n" + " WRONG!! Plz Enter A Valid Letter " + "\n" )
     continue;
-   }
-    // This line of code will print out whatever is inputted in by the user.
-  //  console.log("THE USER INPUTTED:", userInput + "\n" );
-
-   if (checkWinner( allV.randWord,spaceRepeat) || !allV.numTries ) {
+    }
+   if (checkWinner( allV.randWord , spaceRepeat) || !allV.numTries ) {
      console.log(checkWinner(allV.randWord,spaceRepeat));
      console.log ? " You won !! " : " you lost the game! " ;
      allV.startGame = false;
    }
+
    if(allV.numTries === 1){
-     console.log("YOU HAVE 1 LIVES REMAINING (BE CAREFUL) *** ")
-     continue ;
+     console.log("||||||||||||||||||||||||||||||");
+     console.log("YOU HAVE 1 LIVES REMAINING !!!  *_* ")
+     console.log("||||||||||||||||||||||||||||||");
    }
 
   if (!allV.startGame){
@@ -123,7 +110,8 @@ while(allV.startGame){
  }
 }
 
-console.log("   ---   -      -     -    -   ");
-console.log("   ---   -     - -      --     ");
-console.log("   -     ---  -   -     -      ");
+console.log("   ***  *      **    *   *   ");
+console.log("   ***  *     ****    ***    ");
+console.log("   *    ***  *    *    *     ");
+
 run();

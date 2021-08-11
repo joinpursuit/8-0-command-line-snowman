328d3136f1d7b90c9453aa85c72fc110cadf0977
Fatema Kabir <fatema.kabir@pursuit.org>
2021-08-10 23:20:40 -0400

    let the game begin

--- src/snowman.js
+++ src/snowman.js
@@ -8,98 +8,42 @@ function getRandomWord() {
   return dictionary[index];
 }
 //----------------------------------------------------------------//
+function run() {
+  // This line of code gets a random word. The `word` variable will be a string.
+  const word = "loop"
+  //getRandomWord();
+  let validEntry = "abcdefghijklmnopqrstuvwxyz"
+  let wrongEntryType = "\nNot a letter, please enter a letter!"
+  let endMessage = `\nSorry, better luck next time! The correct word is "${word}".`
+  let winningMessage = `\nYay! The word "${word}" saved your snowman!`
+  let incorrectGuesses = word.length + 1 
+  let lettersGuessed = ""
+  let theBlanks = ""
 
-//checks if userinput is correct 
-function checkGuess (randomWord, guess) { 
-  if (randomWord.includes(guess)) {
-    return true
-  } else {
-    return false
+  for (let i = 0; i < word.length; i++){
+    theBlanks += "_ "
   }
-}
-//the userinput matches 
-//replace the "_" with userinput
-//convert to lowercase
-function charReplacer (guess, word, letterDisplay) {
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === guess) {
-      if (word[i] === guess.toLowerCase()){
-        letterDisplay.splice(i,1,guess)
-      }
-    }
-  }
-}
+    console.log("\nWord: " + theBlanks)   
+    console.log("\nRemaining Incorrect Guesses: " + incorrectGuesses)
+    console.log("\nLetters Guessed: " + lettersGuessed)
 
-let listOfGuessedLetters = "";
-const userName = readline.question(`Enter your name:  `)
-
-
-function run(){
-  const greeting = readline.question(`\nWelcome to Ki Sub & Fatema's Snowman Game World!\n\nPress the 'return' button to play the game!`);
-
-  const word = getRandomWord(); 
-  //generates a random word
-  //console.log(word)
-  let remainingChances = word.length + 1 
-  const letterDisplay = [];
-
-  //converts each letter of the word to "_" / empty blanks 
-  function convertToUnderScore (word) {
-  for (let character of word) {
-    character = "_"
-    letterDisplay.push(character)
-    }
-  }
-  convertToUnderScore (word)
-    //the loop that allows the game to continue
-  while (remainingChances > 0 && letterDisplay.includes("_")) {
-    console.log(String(`\nThis word has ${word.length} characters.\n\n${letterDisplay.join("  ")}`))
-    console.log(`\n\nYou guessed: ${listOfGuessedLetters}`)
-    const userInput = readline.question(`\n\nGuess a letter: `);
-    
-    // console.log(`You Guessed: ${listOfGuessedLetters}`);
-    //check if userinput is correct or wrong
-    //check if userInput is a valid character (userinput === number, multiple letters)
-    //if user guesses wrong letter remaning chances decrement by 1
-    //if user guesses invalid character no change in remaining chances count 
-    //prompt accordingly
-    //if userinput guesses are letters (only) add it to listofguessedletters
-  if (userInput.length > 1 || !isNaN(userInput)) { 
-    console.log(`Not a valid entry, please enter a letter! You have '${remainingChances}' chance(s) left!`)
-  } else if (checkGuess(word, userInput) === true) {
-    console.log(`Yay, that is correct! You have '${remainingChances}' chance(s)!`)  
-    listOfGuessedLetters += userInput + " "
-    charReplacer (userInput, word, letterDisplay) 
-    }
-    else {
-      remainingChances --;
-      console.log(`\nOh, no! That is incorrect!\nYou have '${remainingChances}' chance(s)!`)
-      listOfGuessedLetters += userInput + " "
+//the loop that starts the game + continue the game
+  for (let i = 0; i < word.length; i++){
+//The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
+    let userInput = readline.question("\nPick a letter: ")
+    userInput = userInput.toLowerCase()
+    if(validEntry.includes(userInput) && userInput.length === 1){
+      lettersGuessed += userInput + " "
     } 
-  } 
-  //prompt message for a win or loss 
-  if (letterDisplay.join("") === word) {
-    console.log(`\nYay! You got it, the correct word is "${word}"!!\nCongratulations ${userName}!`)
-    const startNewGame = readline.question(`Do you want to test your luck again?\nIf yes, press 'y'\nIf not, press 'n'`) 
-    if (startNewGame === 'y') {
-      console.log(`\nFeeling lucky ${userName}?! Let the game begin ...`)
-      run()
-    } else {
-      console.log(`\nYou are a great sport ${userName}! Have a good day!`)
+  
+      else {
+           i--
       }
-    }
-  if (remainingChances === 0) {
-      console.log(`\nAlways a next time ${userName}!\nThe correct word is '${word}'.`)
-      const startNewGameWhenLost = readline.question(`Wanna try again?\nIf yes, press 'y'\nIf not, press 'n'`) 
-    if (startNewGameWhenLost === 'y') {
-      console.log(`\nGood Luck ${userName}!`)
-      run()
-    } else {
-      console.log(`\nHave a great day ${userName}!`)
-    }
   }
 }
+     
 run();
+
 
 
 //how can i start / play the game
@@ -115,7 +59,7 @@ run();
 //if incorrectGuess count is 0 the user has no chances left game ends
 //the game ends when the game is lost with an alert "you lost"
 //if the user guesses a wrong character (number or multiple letters)
-//if(userInput typeOf = number || symbol)
+//if(userInput typeOf = number || multiple letters)
 //remaining incorrect guess count doesn't change but an alert pops up saying ... please enter a letter
 
 

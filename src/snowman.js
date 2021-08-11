0f49bf40300915c9007156433dab8867852381c9
Billy Dominique <billydominique@pursuit.org>
2021-08-10 22:39:18 -0400

    Start Menu Created

--- src/snowman.js
+++ src/snowman.js
@@ -1,6 +1,14 @@
 /*
   `readline-sync` is a library that allows you to access user input from the command line. The library is assigned to the variable `readline`. It is used in the `run()` function below.
 */
+   
+//daughter
+//01234567
+
+//0 1 2 3 4 5 6 7 
+//d_a_u_g_h_t_e_r
+//0123456789TETTF
+
 const readline = require("readline-sync");
 /*
   The `dictionary` variable will have an array of words that can be used for your game. It is used in the `getRandomWord()` function.
@@ -22,19 +30,98 @@ function getRandomWord() {
 
   Once you understand the code below, you may remove the comments if you like.
 */
+function gameStatus(divider, remainingGuesses, guessLetters, guessBox){
+  
+
+  console.log(divider)
+  console.log("Lives Left: " + remainingGuesses)
+  console.log("Letters Guessed: " + guessLetters )
+  console.log("Word: " + guessBox)
+}
+
+
+function removeSpaces(guessBox){ 
+
+
+  for(guess of guessBox){
+  guessBox = guessBox.replace(" ","")
+} 
+return guessBox
+}
+
+function lowCase(userInput){
+
+  return userInput.toLowerCase()
+
+}
+
+function letterChecker(userInput){
+  if (userInput.toLowerCase() === 'a' ||
+  userInput.toLowerCase() === 'b' ||
+  userInput.toLowerCase() === 'c' ||
+  userInput.toLowerCase() === 'd' ||
+  userInput.toLowerCase() === 'e' ||
+  userInput.toLowerCase() === 'f' ||
+  userInput.toLowerCase() === 'g' ||
+  userInput.toLowerCase() === 'h' ||
+  userInput.toLowerCase() === 'i' ||
+  userInput.toLowerCase() === 'j' ||
+  userInput.toLowerCase() === 'k' ||
+  userInput.toLowerCase() === 'l' ||
+  userInput.toLowerCase() === 'm' ||
+  userInput.toLowerCase() === 'n' ||
+  userInput.toLowerCase() === 'o' ||
+  userInput.toLowerCase() === 'p' ||
+  userInput.toLowerCase() === 'q' ||
+  userInput.toLowerCase() === 'r' ||
+  userInput.toLowerCase() === 's' ||
+  userInput.toLowerCase() === 't' ||
+  userInput.toLowerCase() === 'u' ||
+  userInput.toLowerCase() === 'v' ||
+  userInput.toLowerCase() === 'w' ||
+  userInput.toLowerCase() === 'x' ||
+  userInput.toLowerCase() === 'y' || 
+  userInput.toLowerCase() === 'z' ){
+    return true
+
+  }
+}
+
+
+// This line of code gets a random word. The `word` variable will be a string.
+/*
+The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
+
+The text that will show up to the user will be "Guess a letter: ". Whatever value is entered will be assigned to the variable `userInput`.
+
+After a user hits the 'return' key, the rest of the code will run.
+*/
+// create an array
+// 
+// put in "_" in each index of the array and the array should match the length of the word
+// if user guesses incorrectly remainingGuesses -= 1
+//
 function run() {
-  // This line of code gets a random word. The `word` variable will be a string.
+  // random word to be guessed
   const word = getRandomWord();
-  /*
-    The line of code below stops the execution of your program to ask for input from the user. The user can enter whatever they want!
+  // Separates user inputs with a space for better visibility 
+  let divider = " \n "
+  // array used for infinite loop until game is won or lost 
+  let array = [0]
+  //number of chances left to guess
+  let remainingGuesses = word.length + 1
 
-    The text that will show up to the user will be "Guess a letter: ". Whatever value is entered will be assigned to the variable `userInput`.
+  let guessBox = ""
+  let guessLetters = ''
+   // console.log(word)
+  for (let letter of word){
+    guessBox +=  "_ "
+  }
 
-    After a user hits the 'return' key, the rest of the code will run.
-  */
-  const userInput = readline.question("Guess a letter: ");
-  // This line of code will print out whatever is inputted in by the user.
-  console.log("THE USER INPUTTED:", userInput);
+  gameStatus(divider, remainingGuesses, guessLetters, guessBox)
+  
+  const userInput = readline.question("Choose Wisely: ");
+    
 }
 
 run();

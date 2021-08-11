c4b745a2cc5531553bad0c4fd0b890e0773147b8
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-08-10 22:26:47 -0400

    improved for readability. Final Submission

--- src/snowman.js
+++ src/snowman.js
@@ -3,99 +3,97 @@ const readline = require("readline-sync");
 
 const dictionary = require("./dictionary");
 
+//HELPER FUNCTIONS
+
+//Generates a random word.
 function getRandomWord() {
   const index = Math.floor(Math.random() * dictionary.length);
   return dictionary[index];
 }
-
 /**
- * 
- * @param {string} generatedNumber - string chosen by the computer
- * @param {*} underscoreStr - underscores string
- * @param {*} letter - letter from user input
- * @returns either the modified underscores string or false if letter is not included in the hidden word.
- */
-
- function replaceUnderscores(generatedNumber,underscoreStr,letter) {
-  if (generatedNumber.includes(letter)) {
-    underscoreStr = underscoreStr.split(" ");
-    for (let i = 0; i < generatedNumber.length; i++) {
-      if (generatedNumber[i] === letter) {
-         underscoreStr.splice(i, 1, letter);
-      }
-    }
-    return underscoreStr.join(" ");
-  }
-    else return false;
-}
-
-/**
- * 
  * @param {string} hiddenWord - hidden word
  * @param {string} underscoreStr - underscores string
- * @returns boolean- true if user wins or false
+ * @returns {boolean} true if user wins or false
  */
+
 function checkWinner(hiddenWord, underscoreStr) {
   underscoreStr = underscoreStr.split(" ").join("");
   return hiddenWord === underscoreStr;
 }
-
-
 /**
  * @param {string} user input - word that gets inputted by player
- * @returns typeOf - checks several conditions
+ * @returns {typeOf} - checks several conditions
  */
+
 function guessCheck(userInput){
     return typeof userInput === "string" && userInput.length === 1
            && (userInput >= "a" && userInput <= "z" );  
 }
+/**
+ * @param {string} generatedWord - a random word chosen by the computer
+ * @param {string} underscoreStr - underscores string
+ * @param {string} letter - letter from user input
+ * @returns {boolean}
+ */
+
+function replaceUnderscores(generatedWord,underscoreStr,letter) {
+if (generatedWord.includes(letter)) {
+underscoreStr = underscoreStr.split(" ");
+for (let i = 0; i < generatedWord.length; i++) {
+if (generatedWord[i] === letter) {
+underscoreStr.splice(i, 1, letter);
+}
+}
+return underscoreStr.join(" ");
+}
+else return false;
+}
+
+//*** Snowman game ***
 
 function run() {
 
-  console.log(` ________________________________________
+  console.log(`----------------------------------------
+ ❄️       ❄️      _==_     ❄️     ❄️     ❄️
+     ❄️          (",) ❄️            ❄️ 
+  ❄️      ❄️      /. \\        ❄️         ❄️  
+     ❄️     ____( :  )___ ❄️         ❄️  
+-----------------------------------------
+    ☃️   RONNIE'S SNOW MAN GAME  ☃️`)
 
-      ☃️  PURSUIT SNOW MAN GAME  ☃️
+let startGame = true;
+let numberOfTries = 8;
+let inputStr = "";   
+const word = getRandomWord(); 
+let wordInUnderScores = "_ ".repeat(word.length);
+console.log(wordInUnderScores+ "\n"); 
 
- ----------------------------------------
- ❄️       ❄️      _==_     ❄️     ❄️   ❄️
-     ❄️          (",) ❄️           ❄️
-  ❄️      ❄️      /. \\        ❄️        ❄️  
-    ❄️      ____( :  )___❄️        ❄️  
------------------------------------------`)
 
-  let startGame = true;
-  let numberOfTries = 7;
-  let inputStr = "";   // keeps track of the user's inputs
-  const word = getRandomWord(); // This line of code gets a random word.
-  // console.log(word); // ** DISPLAYS HIDDEN WORD WHEN TURNED ON **
-  let wordInUnderScores = "_ ".repeat(word.length); // displays an underscore in place of each letter from our hidden word
-  console.log(wordInUnderScores+ "\n"); // shows the player how many underscores
-    while(startGame) {
-        console.log("Guessed letters: ", inputStr); // print guessed letters
-        console.log(`You have ${numberOfTries} guesses remaining`); // print remaining guesses 
-        const userInput = readline.question(`Enter your guess: `); 
-        inputStr += " " + userInput; // adds user input to inputStr
-        if (guessCheck(userInput)){
-        let checkIfBoolean = replaceUnderscores(word, wordInUnderScores, userInput);  // checks for a true or false value
-        if (!checkIfBoolean) {
-          numberOfTries--;  //decrements the amount of tries every time a guess is made
-        } else {
-          wordInUnderScores = checkIfBoolean;
-          console.log(wordInUnderScores);
-        }
-      
-        } else {
-          console.log("Please enter a valid letter");
-          continue;
-        }
-        if (checkWinner(word, wordInUnderScores) || !numberOfTries) {
-        console.log(checkWinner(word, wordInUnderScores) ? "You won!" : `You lost! the correct guess was: ${word}`);
-        startGame = false;
-      }
-  }
-        if (!startGame){
-        let continuePlaying = readline.question("Play again? (y / n): ").toLowerCase();
-        if(continuePlaying !== "n" && continuePlaying !== "no") {
+while(startGame) {
+console.log("Guessed letters: ", inputStr); 
+console.log(`You have ${numberOfTries} tries remaining`); 
+const userInput = readline.question(`Enter your guess: `); 
+inputStr += " " + userInput;
+if (guessCheck(userInput)){
+let letterCheck = replaceUnderscores(word, wordInUnderScores, userInput);  
+if (!letterCheck) {
+numberOfTries--;  
+} else {
+  wordInUnderScores = letterCheck;
+  console.log(wordInUnderScores);
+  }      
+} else {
+  console.log("\nNumbers & symbols are not allowed!\n");
+  continue;
+}
+  if (checkWinner(word, wordInUnderScores) || !numberOfTries) {
+  console.log(checkWinner(word, wordInUnderScores) ? "You won! 🏆" : `You lost! the correct guess was: ${word}`);
+  startGame = false;
+ }
+}
+  if (!startGame){
+  let continuePlaying = readline.question("Play again? (y / n): ").toLowerCase();
+  if(continuePlaying !== "n" && continuePlaying !== "no") {
      run()
    }
   }

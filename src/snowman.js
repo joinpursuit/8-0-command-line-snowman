711dc852eccf0f483c108850c28fbf42ef905f76
“Lili <lilyhuang114classb@gmail.com>
2021-08-10 20:18:31 -0400

    Updated version with comments.

--- src/snowman.js
+++ src/snowman.js
@@ -39,7 +39,7 @@
 
 // run();
 
-//------------------------------------------- LET'S START FROM HERE -------------------------------------------
+//-------------------------------------------------- LET'S START FROM HERE --------------------------------------------------
 
 const readline = require("readline-sync"); // Declare a variable 'readline' which enable you get the userInput from the command line. 
 const dictionary = require("./dictionary"); // Declare a variable 'dictionary' which is used for generate the random words.
@@ -77,10 +77,10 @@ function checkInput (word,userInput){
 
 // Third helper function:
 // The format exchanger between before and after guessing 
-  // The parameter 'storage' should be relative with the argument (the orginal format) 
+  // The parameter 'storage' should be related with the argument (the orginal format) 
   // -->'underscore' without space which set up in the main function.
 function replacedLetters(word, storage ,userInput) {
-  // The emptyStrArr is the basic format exchanger
+  // The inputExchanger is the basic format exchanger
   let inputExchanger = storage.split("");
   for(let i=0; i < word.length; i++) {
     // Loop through every letter in the word to check if it equals to the userInput 
@@ -110,36 +110,59 @@ function chancesLeft(word,userInput,guessChances) {
 function run() {
   // Declare a variable 'word' equals to the value, which is calling the getRandomWord function.
   const word = getRandomWord();
+  // Declare a variable 'underscore' which according to the word's length to repeat "_"
   let underscore = "_".repeat(word.length);
+  // Prints the format before guessing
   console.log(underscore.split("").join(" ") + "\n");
+  // Declare a variable 'guessChances' which means how many times could guess in total (before guessing)
   let guessChances = word.length + 1;
+  // While Loop if guessChances is greater than 0
   while (guessChances > 0) {
+    // Print: the reminder of the length of word and the userInput 
     console.log("Reminder: This secrect word should have " + word.length + " lowercase letters." + "\n");
     const userInput = readline.question("Guess a letter: ");
     console.log("\n" + "THE USER INPUTTED:", userInput + "\n");
+    // If the userInput is a lowercase letter, then continue to check, otherwise 
+      // --> Prints "Please enter a valid letter!" and go back to the beginning of the while loop
     if (checkCorrectInput(userInput)){
+      // Calling the function
+      // --> re-assign the value to the variable 'underscore' which replace "_" with the userInput
       underscore = replacedLetters(word, underscore, userInput);
+      // To check the userInput is the exactly the same with the letter of the word
       if (checkInput (word,userInput)){ 
+        // If true, prints the updated format
         console.log(underscore + "\n" );
         console.log("Good job! Keep going!" + "\n")
 
       } else {
+        // If false, calling the function
+        // --> re-assign the value to the variable 'guessChances'
         guessChances = chancesLeft(word,userInput,guessChances)
+        // Prints the updated underscore above which without any changing
         console.log(underscore + "\n");
+        // Prints "Incorrect! Please try again!"
         console.log("Incorrect! Please try again!" + "\n")
+        // Prints the updated guessChances which would decrease by one after each incorrect userInput
         console.log("Chances less than " + guessChances + "\n");
       } 
+      // Re-assign the value (the final format) to the variable 'underscore' 
       underscore = underscore.split(" ").join("");
+      // If the final format underscore equals to the random word
+        // Prints the congrat sentence.
       if (underscore ===  word) {
         console.log("Congratulations! You Win!")
+        // Stop the game and go back to the beginning of the while loop
         guessChances = 0;
         continue;
       }
     } 
     else {
+      // If the userInput is not a lowercase letter 
+      // --> Prints "Please enter a valid letter!" and go back to the beginning of the while loop
         console.log("Please enter a valid letter!" + "\n");
         continue;
     }
+    // If no more guessChances, prints "Sorry, you lost! Take a break and try again!"
     if(!guessChances){
       console.log("Sorry, you lost! Take a break and try again!")
     }

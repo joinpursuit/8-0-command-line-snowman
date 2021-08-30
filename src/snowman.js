d9bff5bcbd8bc52f5795d54e15e5cb2b75de7f2a
salrey <tekdehn@gmail.com>
2021-08-30 06:40:27 -0400

    Add helper function to reset values

--- src/snowman.js
+++ src/snowman.js
@@ -66,6 +66,25 @@ function guessesLeft (guesses, word, input) {
   return guesses;
 };
 
+//Reset values using destructuring for Function definitions
+function resetValues (global) {
+  switch(global) {
+    case Array.isArray(global) : 
+      return global = [];
+    case typeof global === "string" : 
+      return global = "";
+    case typeof global === "number" :
+      return global = 0;
+    case typeof global === "boolean" :
+      return global = false;
+    case global === word :
+      global = "";
+      return global = getRandomWord();
+    case global === guesses :
+      return global = process.argv[2] || word.length;
+  };
+}; 
+
 
 //----------- GLOBAL VARIABLES----------------
 // Set up global variables to access throughout
@@ -79,9 +98,9 @@ let hiddenArray = [];
 let hiddenWord = "";
 let scoreWins = 0;
 let scoreLosts = 0;
-let wordGuess = 3
-let remainingWordGuess = 0
-let dashCount = 0
+let wordGuesses = 3;
+let remainingWordGuess = 0;
+let dashCount = 0;
 let guessedWord = false;
 
 
@@ -91,35 +110,41 @@ function run() {
   //Check the word to test
   // console.log(word)
 
-  console.log(chalk.blue("----------The Snowman Game----------\n\n Remaining Incorrect Letter Guesses: "  + guesses + "\n " + hiddenLetters(word) + "\n\n")) 
+  console.log(chalk.purple("----------Command Line Snowman Game----------\n\n"));
+
+  if (!scoreWins) {
+    console.log(chalk.green("Game Play: Guess the word below. First, try guessing by letter. However, each time you guess a letter incorrectly, you lose a try to guess. At a certain point in the game, there's an option to guess the whole word. At the end of each game, you may continue playing new games. Once you're done, you'll see how many times you won and lost throughout the series. \n\n"));
+  };
+
+  console.log(chalk.purple("----------The Snowman Game----------\n\n Remaining Incorrect Letter Guesses: "  + guesses + "\n " + hiddenLetters(word) + "\n\n")); 
   
   //While loop until word is complete or there are no more letter guesses left
   while (word !== hiddenWord && guesses) {
     // Check if it's time to guess word
-    dashCount = hiddenArray.filter(letter => letter === "_").length
-      while (dashCount <= 2 && wordGuess) {
-        let makeGuess = readline.question("Would you like to guess the word? You have " + wordGuess + " tries. Y/N?")
-        makeGuess = makeGuess.toLowerCase()
+    dashCount = hiddenArray.filter(letter => letter === "_").length;
+      while (dashCount <= 2 && wordGuesses) {
+        let makeGuess = readline.question("Would you like to guess the word? You have " + wordGuesses + " tries. Y/N?");
+        makeGuess = makeGuess.toLowerCase();
         if (makeGuess === 'y') {
-          let wordInput = readline.question(" Guess the word: ")
+          let wordInput = readline.question(" Guess the word: ");
           wordInput = wordInput.toLowerCase();
           if (wordInput === word) {
-            console.log(`That's correct! You've won! You took ${remainingWordGuess} word guesses and ${guessCount} letter guesses`)
-            hiddenWord = wordInput
-            scoreWins++
+            console.log(`That's correct! You've won! You took ${remainingWordGuess} word guesses and ${guessCount} letter guesses`);
+            hiddenWord = wordInput;
+            scoreWins++;
             guessedWord = true;
             break;
           } else {
-            if (wordGuess > 0) {
-              wordGuess--
-              remainingWordGuess++
-              console.log(`That's incorrect. Try again!`)
-            }
-          }
+            if (wordGuesses > 0) {
+              wordGuesses--;
+              remainingWordGuess++;
+              console.log(`That's incorrect. Try again!`);
+            };
+          };
         } else {
           break;
-        }
-      }
+        };
+      };
 
     // Switch to letter guesses if you haven't guessed the word 
     if (guessedWord) {
@@ -127,64 +152,64 @@ function run() {
     } else {
       let userInput = readline.question(" Guess a letter: ");
       // set to lower case to accept values in uppercase as well
-      userInput = userInput.toLowerCase()
+      userInput = userInput.toLowerCase();
   
       //Check for Errors
       while (checkInput(userInput)) {
-        userInput = readline.question(" Guess a letter: ")
-      }
+        userInput = readline.question(" Guess a letter: ");
+      };
       
       //Count number of guesses taken
       if (guessesLeft(guesses, word, userInput) !== guesses) {
-        guessCount += 1 
-      }
+        guessCount += 1; 
+      };
   
       //Update remaining incorrect guesses
-      guesses = guessesLeft(guesses, word, userInput)
+      guesses = guessesLeft(guesses, word, userInput);
       
       //Update letters guessed
-      lettersGuessed = lettersList(userInput)
+      lettersGuessed = lettersList(userInput);
   
       //Update hidden word
-      hiddenWord = hiddenLetters(word, userInput)
+      hiddenWord = hiddenLetters(word, userInput);
   
-      console.log("-----------------------------------")
-      console.log("\nRemaining Incorrect Guesses: " + guesses + "\nLetters Guessed: " + lettersGuessed + "\n Word: " + hiddenWord + "\n\n")
-      
-    }
-  }
+      console.log("-----------------------------------");
+      console.log("\nRemaining Incorrect Guesses: " + guesses + "\nLetters Guessed: " + lettersGuessed + "\n Word: " + hiddenWord + "\n\n");
+  
+    };
+  };
   
   //Check if user won or lost
   if (word === hiddenWord && !guessedWord) {
-    console.log("You win! You took " + guessCount + " guesses.\n")
-    scoreWins++
+    console.log("You win! You took " + guessCount + " guesses.\n");
+    scoreWins++;
   } else if (word !== hiddenWord && !guessedWord) {
-    console.log("You lost! The correct word is " + word + ".\n")
-    scoreLosts++
-  }
+    console.log("You lost! The correct word is " + word + ".\n");
+    scoreLosts++;
+  };
 
   // Give user the option to replay and if yes, reset all applicable variables 
-  console.log("Would you like to play another game?")
-  let userChoice = readline.question("Enter Y/N ")
-  userChoice = userChoice.toLowerCase()
+  console.log("Would you like to play another game?");
+  let userChoice = readline.question("Enter Y/N ");
+  userChoice = userChoice.toLowerCase();
 
   if (userChoice === 'y') {
-    word = ""
+    word = "";
     word = getRandomWord();
-    guesses = process.argv[2] || word.length  
-    guessCount = 0
-    checkArray = []
-    letterArray = []
-    hiddenArray = []
-    hiddenWord = ""
-    remainingWordGuess = 0
-    dashCount = 0
+    guesses = process.argv[2] || word.length;  
+    guessCount = 0;
+    checkArray = [];
+    letterArray = [];
+    hiddenArray = [];
+    hiddenWord = "";
+    remainingWordGuess = 0;
+    dashCount = 0;
     guessedWord = false; 
-    run()
+    run();
   } else {
-    console.log(`Great! You've won ${scoreWins}x and lost ${scoreLosts}x.`)
-  }
+    console.log(`Great! You've won ${scoreWins}x and lost ${scoreLosts}x.`);
+  };
 
-}
+};
 
 run();
